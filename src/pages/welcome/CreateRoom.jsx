56ef2b25ce76513f7efc644a970d5c6892590c4d
Berat BAYRAM <beratbayram7@gmail.com>
2021-12-25 19:14:47 +0300

    Create and join room is done

--- src/pages/welcome/CreateRoom.jsx
+++ src/pages/welcome/CreateRoom.jsx
@@ -1,13 +1,41 @@
 import {usePortals} from "react-portal-hook";
 import Modal from "../../elements/Modal";
+import axios from "axios";
+
+function CreateRoomModal() {
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const nickName = event?.target?.elements?.inputNickname?.value
+        const uniqueKey = nickName.length;
+        const payload = {
+            uniqueKey: uniqueKey,
+            nickName: nickName
+        }
+        try {
+            const responseFromCreateChatter = await axios.post('http://localhost:8080/createChatter/', payload)
+            const responseFromCreateRoom = await axios.post('http://localhost:8080/createRoom/', uniqueKey)
+            console.log(responseFromCreateRoom)
+        }catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="inputNickname">Nick Name</label>
+            <input id="inputNickname" name="inputNickname"/>
+            <button type="submit">Create</button>
+        </form>
+    )
+}
 
 export default function CreateRoom() {
-    const portalManager = usePortals();
+    const {open: openPortal} = usePortals();
 
     return (
         <div id="CreateRoom">
             <button onClick={() =>
-                portalManager.open(portal => <Modal title="Hello" closeModal={portal.close}>Q</Modal>)
+                openPortal(p => <Modal title="Create Room" closeModal={p.close}><CreateRoomModal/></Modal>)
             }>Create A Room
             </button>
         </div>
--- src/pages/welcome/JoinRoom.jsx
+++ src/pages/welcome/JoinRoom.jsx
@@ -1,7 +1,51 @@
+import {usePortals} from "react-portal-hook";
+import Modal from "../../elements/Modal";
+import axios from "axios";
+
+function JoinRoomModal() {
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const nickName = event?.target?.elements?.inputNickname?.value
+        const roomId = event?.target?.elements?.inputRoomId?.value
+        const uniqueKey = nickName.length;
+        const payload = {
+            uniqueKey: uniqueKey,
+            nickName: nickName
+        }
+        const payload2 = {
+            uniqueKey: uniqueKey,
+            roomId: roomId
+        }
+
+        try {
+            const responseFromCreateChatter = await axios.post('http://localhost:8080/createChatter/', payload)
+            const responseFromJoinRoom = await axios.post('http://localhost:8080/joinRoom/', payload2)
+            console.log(responseFromJoinRoom)
+        }catch (error) {
+            console.error(error)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="inputNickname">Nick Name</label>
+            <input id="inputNickname" name="inputNickname"/>
+            <label htmlFor="inputRoomId">Room Id</label>
+            <input id="inputRoomId" name="inputRoomId"/>
+            <button type="submit">Create</button>
+        </form>
+    )
+}
+
 export default function JoinRoom() {
+    const {open: openPortal} = usePortals();
     return (
         <div id="JoinRoom">
-            <button>Join a Chat</button>
+            <button onClick={() =>
+                openPortal(p => <Modal title="Join Room" closeModal={p.close}><JoinRoomModal/></Modal>)
+            }>Join A Room
+            </button>
         </div>
     )
+
 }
