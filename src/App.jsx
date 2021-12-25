53da76363e2c75712c4e09aa40122bb227787842
Berat BAYRAM <beratbayram7@gmail.com>
2021-12-25 16:28:30 +0300

    modal init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import './App.sass';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {PortalProvider} from "react-portal-hook";
 import Welcome from './pages/welcome/Welcome';
 import bg from './assets/bg.svg';
 import Test from './Test';
 
-
 function App() {
-  return (
-    <BrowserRouter>
-      <div id="site">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </div>
-      <img src={bg} alt="background" id="background"/>
-    </BrowserRouter>
-  )
+    return (
+        <PortalProvider>
+            <BrowserRouter>
+                <div id="site">
+                    <Routes>
+                        <Route path="/" element={<Welcome/>}/>
+                        <Route path="/test" element={<Test/>}/>
+                    </Routes>
+                </div>
+                <img src={bg} alt="background" id="background"/>
+            </BrowserRouter>
+        </PortalProvider>
+    )
 }
+
 export default App;
--- src/pages/welcome/CreateRoom.jsx
+++ src/pages/welcome/CreateRoom.jsx
@@ -1,15 +1,30 @@
 import {useState} from 'react';
-import Modal from '../../elements/Modal';
+import {usePortals} from "react-portal-hook";
+
+function Modal({title, children, closeModal}) {
+    return (
+        <div id="modal">
+            <header>
+                <h2>{title}</h2>
+                <button type="button" onClick={closeModal}>X</button>
+            </header>
+            <main>
+                {children}
+            </main>
+        </div>
+    )
+}
 
 export default function CreateRoom() {
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const portalManager = usePortals();
 
     return (
         <div id="CreateRoom">
-            <button onClick={() => setIsModalOpen(!isModalOpen)}>Create A Room</button>
-            <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} title="Create Room">
-                <p>Send nude pls</p>
-            </Modal>
-        </div >
+            <button onClick={() =>
+                portalManager.open(portal => <Modal title="Hello" closeModal={portal.close}>Q</Modal>)
+            }>Create A Room
+            </button>
+        </div>
     )
 }
