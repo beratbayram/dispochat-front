6abe15524f8dacbb70c7b5b8214672616c9fa5be
Berat BAYRAM <beratbayram7@gmail.com>
2022-01-13 21:41:04 +0300

    Aside info is fixed

--- src/pages/room/RoomAside.jsx
+++ src/pages/room/RoomAside.jsx
@@ -6,44 +6,48 @@ import Api from "../../utils/Api";
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 
-function ToastifyButtons({msgData,closeToast}) {
-    const {message,messageResponseType,chatter} = msgData;
-    async function handleClick(isAccept){
-        closeToast();
-        await toastifyPromise(Api.guestRequest(isAccept));
-    }
-
-    if(!message.match(/wants to join to your room/g) || messageResponseType === 'ERROR') return message;
-    return (
-        <>
-            <p>{message}</p>
-            <br/>
-            <p><em>{`(${chatter.country}/${chatter.city})`}</em></p>
-            <button onClick={() => handleClick(true)} id="toast-button-accept">Accept</button>
-            <button onClick={() => handleClick(false)} id="toast-button-reject">Reject</button>
-        </>
-    )
-}
-
-async function checkGuests(/*event*/) {
-    await toastifyPromiseConfirm(Api.fetchRequester(),ToastifyButtons);
+async function checkGuests(roomId, setChatters, ToastifyButtons) {
+    await toastifyPromiseConfirm(Api.fetchRequester(), ToastifyButtons);
 }
 
 export default function RoomAside({nickName, roomId}) {
     const navigate = useNavigate();
-    const [chatters,setChatters] = useState(null)
-    const {guestChatter,ownerChatter} = chatters ?? {};
+    const [chatters, setChatters] = useState(null)
+    const {guestChatter, ownerChatter} = chatters ?? {};
 
-    useEffect(() => { Api.queryChatters(roomId).then(setChatters)},[roomId]);
+    function ToastifyButtons({msgData, closeToast}) {
+        const {message, messageResponseType, chatter} = msgData;
+
+        async function handleClick(isAccept) {
+            closeToast();
+            const {messageResponseType} = await toastifyPromise(Api.guestRequest(isAccept));
+            if (messageResponseType === 'SUCCESS')
+                setChatters(await Api.queryChatters(roomId));
+        }
+
+        if (!message.match(/wants to join to your room/g) || messageResponseType === 'ERROR') return message;
+        return (
+            <>
+                <p>{message}</p>
+                <br/>
+                <p><em>{`(${chatter.country}/${chatter.city})`}</em></p>
+                <button onClick={() => handleClick(true)} id="toast-button-accept">Accept</button>
+                <button onClick={() => handleClick(false)} id="toast-button-reject">Reject</button>
+            </>
+        )
+    }
+
+    useEffect(() => {
+        Api.queryChatters(roomId).then(setChatters)
+    }, [roomId]);
 
 
     async function handleKillSwitch() {
         // FIXME
         // eslint-disable-next-line no-restricted-globals
-        if(confirm("Are u sure you want to kill this room?"))
-        {
-            const {message,messageResponseType} = await toastifyPromise(Api.killSwitch());
-            if(messageResponseType === 'SUCCESS') navigate('/');
+        if (confirm("Are u sure you want to kill this room?")) {
+            const {/*message,*/messageResponseType} = await toastifyPromise(Api.killSwitch());
+            if (messageResponseType === 'SUCCESS') navigate('/');
         }
     }
 
@@ -54,7 +58,8 @@ export default function RoomAside({nickName, roomId}) {
                 <h1><em>Dispo</em>Chat</h1>
             </div>
             <div id="aside-middle">
-                <h2>{`Connected to room ${roomId}`}</h2>
+                <h2>Connected to</h2>
+                <h2>{`room ${roomId}`}</h2>
                 <table>
                     <tbody>
                     <tr>
@@ -77,7 +82,9 @@ export default function RoomAside({nickName, roomId}) {
                 </table>
             </div>
             <div id="aside-bottom">
-                <button onClick={checkGuests} type="button">
+                <button
+                    onClick={(event) => checkGuests(roomId, setChatters, ToastifyButtons)}
+                    type="button">
                     Check guests
                 </button>
                 <button onClick={handleKillSwitch} type="button">
